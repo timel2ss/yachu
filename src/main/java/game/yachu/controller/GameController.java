fe512dbfacf1536c40e24202454148d972ada3e3
timel2ss <dyddn0823@hanyang.ac.kr>
2022-09-12 17:16:43 +0900

    refactor: GameController 분리 -> (GameController, RecordController)

--- src/main/java/game/yachu/controller/GameController.java
+++ src/main/java/game/yachu/controller/GameController.java
@@ -1,15 +1,12 @@
 package game.yachu.controller;
 
 import game.yachu.controller.request.GainRequest;
-import game.yachu.controller.request.RecordRequest;
 import game.yachu.controller.request.RollRequest;
 import game.yachu.controller.response.GainResponse;
 import game.yachu.controller.response.LoadResponse;
 import game.yachu.controller.response.RollResponse;
 import game.yachu.domain.*;
 import game.yachu.repository.GameStateRepository;
-import game.yachu.repository.RecordRepository;
-import game.yachu.repository.dto.Record;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,12 +21,10 @@ import java.util.stream.Collectors;
 public class GameController {
 
     private final GameStateRepository gameStateRepository;
-    private final RecordRepository recordRepository;
 
     @Autowired
-    public GameController(GameStateRepository gameStateRepository, RecordRepository recordRepository) {
+    public GameController(GameStateRepository gameStateRepository) {
         this.gameStateRepository = gameStateRepository;
-        this.recordRepository = recordRepository;
     }
 
     @GetMapping("/game/{id}")
@@ -86,20 +81,5 @@ public class GameController {
         }
         player.resetState();
         return new GainResponse(player.getScore(), false);
-    }
-
-    @ResponseBody
-    @GetMapping("/api/record")
-    public List<Record> findTop10() {
-        return recordRepository.findTop10();
-    }
-
-    @ResponseBody
-    @PostMapping("/api/record/new")
-    public Long save(@RequestBody RecordRequest request) {
-        Record record = new Record(request.getNickname(), request.getScore());
-        recordRepository.save(record); // record id 삽입
-        log.info("Record Save - id={}", record.getId());
-        return record.getId();
     }
 }
