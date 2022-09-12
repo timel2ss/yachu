d39096503abbef57cdb2a5a1a37aba510b634ff3
timel2ss <dyddn0823@hanyang.ac.kr>
2022-09-12 20:22:18 +0900

    feat: session 관련 예외 작업

--- src/main/java/game/yachu/controller/interceptor/SessionInterceptor.java
+++ src/main/java/game/yachu/controller/interceptor/SessionInterceptor.java
@@ -1,5 +1,6 @@
 package game.yachu.controller.interceptor;
 
+import game.yachu.exception.SessionMismatchException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,7 +15,8 @@ import static org.springframework.web.servlet.HandlerMapping.URI_TEMPLATE_VARIAB
 public class SessionInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("session interceptor: requestURI={}", request.getRequestURI());
+        String requestURI = request.getRequestURI();
+        log.info("session interceptor: requestURI={}", requestURI);
 
         Map<String, String> pathVariables = (Map<String, String>) request.getAttribute(URI_TEMPLATE_VARIABLES_ATTRIBUTE);
         log.info("PathVariables: {}", pathVariables);
@@ -27,7 +29,11 @@ public class SessionInterceptor implements HandlerInterceptor {
         }
 
         log.info("Invalid Access");
-        response.sendRedirect("/");
-        return true;
+        if (requestURI.startsWith("/game/")) {
+            response.sendRedirect("/");
+            return true;
+        }
+
+        throw new SessionMismatchException();
     }
 }
