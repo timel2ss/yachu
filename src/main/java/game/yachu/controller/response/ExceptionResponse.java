8dbfbfcad869cb63688ab4935848f80912605038
timel2ss <dyddn0823@hanyang.ac.kr>
2022-09-12 20:20:26 +0900

    feat: 예외처리 controller 구현

---
+++ src/main/java/game/yachu/controller/response/ExceptionResponse.java
@@ -0,0 +1,13 @@
+package game.yachu.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String message;
+    private String statusCode;
+}
